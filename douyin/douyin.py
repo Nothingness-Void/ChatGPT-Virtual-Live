15c278408677eed2a9aa9239c7dade0b6b4879c4
Nothingness-Void <55913486+Nothingness-Void@users.noreply.github.com>
2023-08-13 02:05:26 +0800

    更改

--- douyin/douyin.py
+++ douyin/douyin.py
@@ -12,35 +12,50 @@ import websockets
 import json
 import requests
 
+# ChatGPT API的URL和密钥
+bot_api_url = "https://openaiapi.elecho.top/v1/chat/completions" # openai的api链接
+bot_api_key = "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" # 填写你的api-key
+
+# ChatGPT参数
+chatgpt_params = {
+    "model": "gpt-3.5-turbo",
+    "messages": [{"role": "system", "content": "现在你正在哔哩哔哩直播，你的名字叫XX，你是由XXX制造的AI模型，后面的聊天将会是b站用户和你的互动，且每一条消息与上一条都没有联系，你的回答要尽量简短。"}],# 设置ai预设
+    "max_tokens": 1000,# 设置单次回复量（最大）
+    "temperature": 0.7,
+    "n": 1,
+    "stop": "\n"
+}
+
 async def get_data():
     async with websockets.connect('ws://127.0.0.1:8888',ping_interval =None) as websocket:
         while True:
         # 接收消息
             message = await websocket.recv()
             message =  json.loads(message,strict=False)
+
             if(message['Type']==1):
                 msg =  json.loads(message['Data'],strict=False)
-                #print(msg['Content'])
-                text= msg['Content']
-                openai.api_key = ""
-                url = "https://api.openai.com/v1/engines/davinci-codex/completions"
-                messages =  [ {"role": "system", "content": "你是虚拟主播初音酱，你会对用户的输入做出可爱的回应，并对你回复的观众施加可爱魔法，回答字数不要超过100字"},{"role": "user", "content": text}]
-
-                response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-                answer = str(response['choices'][0]['message']['content'])
-                print(answer)
-                putvoice(answer)
-                
-            if(message['Type']==3):
-                msg =  json.loads(message['Data'],strict=False)
                 nickname = msg['User']['Nickname']
-                answer = "欢迎"+nickname+"来到直播间"
-                print(answer)
-                #await putvoice(answer)
+                print(nickname+":"+msg['Content'])
+                question = msg['Content']
+
+                # 使用ChatGPT与观众进行对话
+                chatgpt_params["messages"].append({"role": "user", "content": question})
+                headers = {
+                    "Content-Type": "application/json",
+                    "Authorization": f"Bearer {bot_api_key}"
+                }
+                response = requests.post(bot_api_url, headers=headers, json=chatgpt_params)
+                response_data = response.json()
+                answer = response_data["choices"][0]["message"]["content"]
+                print(f"[AI回复{nickname}]: {answer}")  # 打印AI回复信息
+
+                putvoice(answer)
+
             if(message['Type']==5):
                 msg =  json.loads(message['Data'],strict=False)
                 nickname = msg['User']['Nickname']
-                answer = "感谢"+nickname+"送到礼物"
+                answer = "感谢"+nickname+"送的礼物"
                 putvoice(answer)
         await asyncio.Future()  # run forever
 def putvoice(answer):
@@ -65,7 +80,7 @@ def putvoice(answer):
 
         # 退出临时语音文件
         pygame.mixer.quit()
-if __name__ == '__main__':
+if __name__ == '__main__': #如果不被调用则并发执行
     asyncio.get_event_loop().run_until_complete(get_data())
 
 
